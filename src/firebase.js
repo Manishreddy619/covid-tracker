2987c85085439a9a306245323b91f38251fdcf02
manishreddy619 <manishreddy916@gmail.com>
2021-04-26 12:39:20 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyB-V6hJfpnfhj67PCm4spDjHaql9eJgGZw',
+	authDomain: 'covid-tracker-ba35a.firebaseapp.com',
+	projectId: 'covid-tracker-ba35a',
+	storageBucket: 'covid-tracker-ba35a.appspot.com',
+	messagingSenderId: '220493162280',
+	appId: '1:220493162280:web:03247306064e8f074e121d',
+	measurementId: 'G-37CXPFPS54',
+};
